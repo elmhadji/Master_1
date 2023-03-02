181d1b38888397c1c986a51f1c193b3776f8ebf5
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2023-03-02 22:07:08 +0100

    updated the CAH algorithm and now is better than befor

--- Data Mining/CAH.py
+++ Data Mining/CAH.py
@@ -1,3 +1,12 @@
+def get_min_list (list_of_substruction):
+    min_element = min(list_of_substruction)
+    index_min_element = list_of_substruction.index(min_element)
+    list_of_min_index = [list_of_substruction.index(min_element)]
+    for index in range(index_min_element + 1 , len(list_of_substruction)):
+        if list_of_substruction[index] == min_element and index - 1 != list_of_min_index[-1]:
+            list_of_min_index.append(index)    
+    return list_of_min_index
+
 def get_median_class (list_of_classes):
     result = [sum (classe)/len(classe) for classe in list_of_classes ]
     return result
@@ -28,16 +37,20 @@ def CAH_algorithme (list_of_classes , type):
     i=0
     while len(list_of_classes) != 1 :
         list_of_substuction = get_subtruction_list(list_of_classes , type)
-        # print(list_of_substuction)D
-        #TODO: to upgrade the algorithm make it slecte all the same min value in one iteration
-        index_of_min_class = list_of_substuction.index(min(list_of_substuction))
-        #saving the neigbor class
-        neigbor_class = list_of_classes[index_of_min_class + 1]
-        #new class
-        neigbor_class = list_of_classes[index_of_min_class] + neigbor_class
-        # deleting the old class
-        del list_of_classes[index_of_min_class + 1]
-        list_of_classes[index_of_min_class] = neigbor_class
+        list_of_min_index = get_min_list(list_of_substuction)
+        print(list_of_min_index)
+        list_of_classes_backup = list_of_classes
+        for index ,index_of_min_class in enumerate(list_of_min_index):
+            #TODO: to upgrade the algorithm make it slecte all the same min value in one iteration
+            #index_of_min_class = list_of_substuction.index(min(list_of_substuction))
+            #saving the neigbor class
+            neigbor_class = list_of_classes[index_of_min_class + 1 - index]
+            #new class
+            neigbor_class = list_of_classes[index_of_min_class - index] + neigbor_class
+            # deleting the old class
+            del list_of_classes[index_of_min_class + 1 -index ]
+            list_of_classes[index_of_min_class - index] = neigbor_class
+                
         new_classes.append(list_of_classes.copy())
     return new_classes
 
@@ -66,7 +79,7 @@ test = [
 # print(test.remove(test[4]))
 # print(test)
 # print(test+test2)
-result = CAH_algorithme(test , 2)
+result = CAH_algorithme(test , 1)
 for index , element in enumerate(result):
     print(index,element)
 
